c2f3bbc54ffc5f4ded61bc7d0f7ae5412535e4a6
matek10000 <mateusz.dybas@microsoft.wsei.edu.pl>
2024-11-19 18:07:11 +0100

    Dodano logikę logowania

---
+++ app/lib/AuthContext.js
@@ -0,0 +1,29 @@
+'use client';
+
+import { createContext, useContext, useEffect, useState } from 'react';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
+import { app } from './firebase.js'; // Importujemy z nazwanym eksportem
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const auth = getAuth(app); // Używamy poprawnie zainicjalizowanego Firebase
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+    return () => unsubscribe();
+  }, [auth]);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
