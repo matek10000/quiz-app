629caca7cdf41f597fcff511bd5c0b5f13524ff1
matek10000 <mateusz.dybas@microsoft.wsei.edu.pl>
2024-12-11 15:08:55 +0100

    Dodanie do Sidebaru panelu quizzów

--- app/components/user/SidebarMenu.js
+++ app/components/user/SidebarMenu.js
@@ -1,7 +1,7 @@
 'use client';
 
 import { useAuth } from '@/app/lib/AuthContext'; // Hook do zarządzania stanem użytkownika
-import { FaUser, FaSignInAlt, FaSignOutAlt, FaHome, FaUserEdit, FaList } from 'react-icons/fa';
+import { FaUser, FaSignInAlt, FaSignOutAlt, FaHome, FaUserEdit, FaList, FaCog } from 'react-icons/fa';
 import Link from 'next/link';
 import UserMenu from './UserMenu'; // Komponent użytkownika z dynamicznym zdjęciem profilowym
 
@@ -26,6 +26,10 @@ export default function SidebarMenu() {
             <Link href="/quizzes" className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded">
               <FaList />
               <span>Lista quizów</span>
+            </Link>
+            <Link href="/quizzes/panel" className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded">
+              <FaCog />
+              <span>Panel Quizów</span>
             </Link>
             <Link href="/user/signout" className="flex items-center gap-2 hover:bg-gray-700 p-2 rounded">
               <FaSignOutAlt />
